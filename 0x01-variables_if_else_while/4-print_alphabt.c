be1b2d9d10c873492beb93119e6c584c1e1b7090
isybliss <isyblissumunna@gmail.com>
2022-09-09 06:24:40 -0700

    Alphabets without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all the lettters of the alphabets in lowercase
+ * except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+

b392bb778872049d2e9d46da3b6abb37f0ca42c6
isybliss <isyblissumunna@gmail.com>
2022-09-13 14:47:36 -0700

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_times_table - print the n times table starting from 0
+ * @n: the number of times table
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n < 0 || n > 15)
+		return;
+	for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (b == 0)
+				{
+					_putchar(c + '0');
+				}
+				else if (c < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c < 100)
+				{
+					_putchar(' ');
+					_putchar((c / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					_putchar((c / 100) + '0');
+					_putchar(((c - 100) / 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				if (b < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+

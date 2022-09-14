a8c2eab01db1c5e822edd832c4245428dcfd5ffd
isybliss <isyblissumunna@gmail.com>
2022-09-14 00:08:14 -0700

    fibonacci

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,47 +2,42 @@
 /**
  * print_times_table - print the n times table starting from 0
  * @n: the number of times table
- * Return: 0
+ * Return: no return
  */
 void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n < 0 || n > 15)
-		return;
-	for (a = 0; a <= n; a++)
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= n; b++)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
 				c = a * b;
-				if (b == 0)
+				_putchar(44);
+				_putchar(32);
+				if (c < 10)
 				{
-					_putchar(c + '0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(c + 48);
 				}
 				else if (c < 100)
 				{
-					_putchar(' ');
-					_putchar((c / 10) + '0');
-					_putchar((c % 10) + '0');
+					_putchar(32);
+					_putchar((c / 10) + 48);
+					_putchar((c % 10) + 48);
 				}
 				else
 				{
-					_putchar((c / 100) + '0');
-					_putchar(((c - 100) / 10) + '0');
-					_putchar((c % 10) + '0');
+					_putchar(((c / 100) % 10) + 48);
+					_putchar(((c / 10) % 10) + 48);
+					_putchar((c % 10) + 48);
 				}
-				if (b < n)
-				{
-					_putchar(',');
-					_putchar(' ');
 			}
 			_putchar('\n');
 		}
 	}
-
+}

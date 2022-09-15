a57f6b42d0b6adc68aea831fe0380092abd590f5
isybliss <isyblissumunna@gmail.com>
2022-09-15 04:32:41 -0700

    multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,11 @@
 
 /**
  * mul - function that multiplies two integers
- * @a: integer to be multiplied
- * @b: integer to be multiplied by
- * @c: value of a multiplied by b
+ * @a: integer to be multiplied by b
+ * @b: integer to be multiplied by a
  * Return: result of multiplication
  */
+
 int mul(int a, int b)
 {
 	return (a * b);

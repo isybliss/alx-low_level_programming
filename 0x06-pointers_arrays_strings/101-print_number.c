fe5b596f9320a175e0784bcdc14bbcabb27c5a59
isybliss <isyblissumunna@gmail.com>
2022-09-22 04:20:31 -0700

    print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,23 +8,20 @@
 
 void print_number(int n)
 {
-	int div = 1;
-	int i;
-	int j;
+	unsigned int n1;
+
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		n1 = n * -1;
 	}
 
-	for (i = 0; n / div > 9; i++, div = div * 10)
-		;
-
-	for (; div >= 1; n = n % div, div = div / 10)
+	if (n1 / 10 != 0)
 	{
-		j = n / div;
-		_putchar('0' + j);
+		print_number(n1 / 10);
 	}
+	_putchar((n1 % 10) + '0');
 }
 

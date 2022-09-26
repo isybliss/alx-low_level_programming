dd6b4332037dc889363d1fd6c2f5bfffd5bf2261
isybliss <isyblissumunna@gmail.com>
2022-09-26 05:12:16 -0700

    memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,13 +8,13 @@
  * Return: pointer to the memory area
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		(s + i) = b;
+		s[i] = b;
 	}
 	return (s);
 }

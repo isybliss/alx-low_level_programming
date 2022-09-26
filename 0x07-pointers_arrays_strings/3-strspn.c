eda70ab85528f674641212446ac1decaac12a754
isybliss <isyblissumunna@gmail.com>
2022-09-26 14:25:00 +0100

    length of String occurence

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strspn - function that gets the length of a prefix substring
+ * _strspn - function that gets the length of occurent of a string
  * @s: string to be looked into
- * @accept: string to be accepted
+ * @accept: character to match
  * Return: length of substring
  */
 
@@ -11,9 +11,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; s[i] != '0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] = s[i]; j++)
+		for (j = 0; accept[j] != s[i] ; j++)
 		{
 			if (accept[j] == '\0')
 				return (i);

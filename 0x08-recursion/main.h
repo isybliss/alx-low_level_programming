fd3744d546a1087747ffbbd173a3483908f4788d
isybliss <isyblissumunna@gmail.com>
2022-09-28 06:40:56 -0700

    Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_check(int i, int n);
 int is_prime_number(int n);
+int prime_check(int f, int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 

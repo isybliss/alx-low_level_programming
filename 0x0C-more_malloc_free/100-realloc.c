25d8e56126583348b5ff2557cbd8df54dca5d1fd
isybliss <isyblissumunna@gmail.com>
2022-10-06 12:23:48 +0100

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,3 +12,38 @@
  * NULL if @ptr is NULL or @new_size == 0 && @ptr != NULL
  */
 
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return(ptr);
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+
+		if (new_ptr == NULL)
+			return(NULL);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return(NULL);
+	}
+
+	if (new_size > old_size && ptr != NULL)
+	{
+		new_ptr = malloc(new_size);
+		if(new_ptr == NULL)
+			return(new_ptr);
+
+		for(i = 0; i < old_size; i++)
+			new_ptr[i] = *((char *)ptr + 1);
+
+		free(ptr);
+	}
+	return (nptr);
+}

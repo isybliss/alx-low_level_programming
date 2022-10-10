663f3a6bd23a3ff3ef97d456143dcf332167c6fd
isybliss <isyblissumunna@gmail.com>
2022-10-10 05:10:48 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef	DOG
+#define DOG
 
 /**
  * struct dog - Attributes of a dog
@@ -13,19 +13,17 @@
 struct dog
 {
 	char *name;
-	float age:
+	float age;
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif

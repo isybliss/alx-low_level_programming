168ac9c8cde422fd6d08fe4d76b7377be41117f0
isybliss <isyblissumunna@gmail.com>
2022-10-10 15:12:24 +0100

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point of program to print the name of the file
+ * it was complied from followed by new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

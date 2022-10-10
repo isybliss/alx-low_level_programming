6059924768f3d6982eaf0c5e7c7c9d9a4ad4c1be
isybliss <isyblissumunna@gmail.com>
2022-10-10 06:06:10 -0700

    structures

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+
+	if (d == NULL)
+		return;
+}

425df95ae688b01e40832f0a5cd233c0189d571f
isybliss <isyblissumunna@gmail.com>
2022-10-13 14:40:53 -0700

    variadic function

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,8 +10,7 @@
 void print_all(const char * const format, ...)
 {
 	va_list a;
-	unsigned int j = 0;
-	unsigned int start = 0;
+	unsigned int j = 0, start = 0;
 	char *p;
 
 	va_start(a, format);
@@ -30,13 +29,13 @@ void print_all(const char * const format, ...)
 			start = 1;
 			printf("%i", va_arg(a, int));
 			break;
-		 case 'f':
+		case 'f':
 			switch (start)
 			{case 1: printf(", "); }
 			start = 1;
 			printf("%f", va_arg(a, double));
 			break;
-		 case 's':
+		case 's':
 			switch (start)
 			{case 1: printf(", "); }
 			start = 1;

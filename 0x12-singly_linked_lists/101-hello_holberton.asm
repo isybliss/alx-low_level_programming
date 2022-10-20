5422523273fe980c1724ba1ec72662be801d3581
isybliss <isyblissumunna@gmail.com>
2022-10-20 21:50:23 +0100

    singly linked list

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,15 @@
+	global _start
+
+	section .text
+_start:	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+	mov rax, 60
+	xor rdi, rdi
+	syscall
+
+	section .data
+msg: db "Hello, Holberton", 10
+msglen: equ $ - msg

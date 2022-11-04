05d3a56e6639c9789346d716f3bb4115b343732a
isybliss <isyblissumunna@gmail.com>
2022-11-04 04:36:11 -0700

    file io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,11 +17,11 @@ void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void check_elf(unsigned char *e_ident);
-unsigned int lit_to_big_endian(unsigned int x);
-void print_entry(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_type, unsigned char *e_ident);
 void close_elf(int elf);
 
 #endif

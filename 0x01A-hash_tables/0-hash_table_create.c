2179245e330e64e53c466830b42b255c205a96a5
isybliss <isyblissumunna@gmail.com>
2023-01-09 11:30:24 +0100

    hash

---
+++ 0x01A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create hash table
+ * @size: size of array
+ * Return: pointer to newly created hash table or
+ * NULL if error occurs
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned int i;
+	// Creates a new HashTable
+	
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
---
+++ 0x01A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "has_tables.h"
+
+/*
+ * hash_djb2 - function to implement djb2 algorithm
+ * @str: string to generate hash value
+ * Return: hash key
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}

ae0e8a47493528787167726a5fe17c6f222044e0
isybliss <isyblissumunna@gmail.com>
2023-01-09 13:58:26 -0800

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,18 +14,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (size == 0)
 		return (NULL);
-
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-
 	ht->size = size;
-
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
+	
 	for (i = 0; i < size; i++)
+	{
 		ht->array[i] = NULL;
+	}
 
 	return (ht);
 }

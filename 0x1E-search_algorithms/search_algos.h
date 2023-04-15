a3b92fc9e9a35f3351eb3d23bc0edb1d6c25c8f6
isybliss <isyblissumunna@gmail.com>
2023-04-15 11:07:58 +0000

    updated search algorithm header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,7 +6,16 @@
 #include <stdlib.h>
 
 void print_array(int *array, size_t inc, size_t size);
-
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: integer
+ * @index: index of the node in the list
+ * @next: pointer to the next node
+ * @express: pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
 typedef struct skiplist_s
 {
 	int n;
@@ -14,6 +23,15 @@ typedef struct skiplist_s
 	struct skiplist_s *next;
 	struct skiplist_s *express;
 } skiplist_t;
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
 
 typedef struct listint_s
 {
